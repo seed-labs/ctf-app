1e347dae42c241fe565a6a172d16f8fad4123433
Tyson Thomas <tysonthomas.90@gmail.com>
2020-10-03 17:26:59 -0400

    Code cleanup

--- server/run.py
+++ server/run.py
@@ -6,5 +6,8 @@ from app import create_app
 
 (app, socketio) = create_app()  # fetch the flask app object and socket.io object
 
+# APP_DEBUG = os.environ["APP_DEBUG"]
+# print("APP_DEBUG: ", APP_DEBUG)
+
 if __name__ == '__main__':
     socketio.run(app, debug=True, host="0.0.0.0")
